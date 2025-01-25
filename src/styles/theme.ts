47923755780cd4ff533967321839e174cab8f2dd
Hyelim Choi <HyelimChoi01@gmail.com>
2025-01-25 22:10:27 +0900

    style: 컬러 팔레트 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,11 +1,87 @@
 import { createTheme } from '@mui/material';
 
-const palette = {};
+const palette = {
+  // blue
+  blue300: '#66BFFF',
+  blue400: '#339FFF',
+  blue500: '#2196f3',
+  blue600: '#005FCC',
+  blue700: '#003F7F',
+  // pink
+  pink300: '#F1A5B5',
+  pink500: '#E3879E',
+  pink600: '#F1A5B5',
+  // purple
+  purple300: '#A1A6FF',
+  purple500: '#7D83FF',
+  purple600: '#5A61CC',
+  // green
+  green300: '#66D2D0',
+  green500: '#00A9A5',
+  green600: '#007D7A',
+  // yellow
+  yellow300: '#FFEB66',
+  yellow500: '#FFDD00',
+  yellow600: '#CCB000',
+  // grey
+  grey100: '#F2F2F2',
+  grey200: '#E0E0E0',
+  grey300: '#BDBDBD',
+  grey400: '#828282',
+  grey500: '#4F4F4F',
+  // etc
+  black: '#333333',
+  white: '#F9F9F9',
+};
 
-const theme = createTheme({
-  palette,
+export const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+
+    primary: {
+      main: palette.blue500,
+      light: palette.blue300,
+      dark: palette.blue600,
+    },
+    secondary: {
+      main: palette.pink500,
+      light: palette.pink300,
+      dark: palette.pink600,
+    },
+    background: {
+      default: palette.white,
+    },
+    text: {
+      primary: palette.black,
+      secondary: palette.grey500,
+    },
+    ...palette,
+  },
 });
 
-export default theme;
+export const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
 
-export type ThemeType = typeof theme;
+    primary: {
+      main: palette.blue500,
+      light: palette.blue300,
+      dark: palette.blue600,
+    },
+    secondary: {
+      main: palette.pink500,
+      light: palette.pink300,
+      dark: palette.pink600,
+    },
+    background: {
+      default: palette.black,
+    },
+    text: {
+      primary: palette.white,
+      secondary: palette.grey300,
+    },
+    ...palette,
+  },
+});
+
+export type ThemeType = typeof lightTheme;
---
+++ src/types/theme.d.ts
@@ -0,0 +1,55 @@
+declare module '@mui/material/styles' {
+  interface Palette {
+    blue300: string;
+    blue400: string;
+    blue500: string;
+    blue600: string;
+    blue700: string;
+    pink300: string;
+    pink500: string;
+    pink600: string;
+    purple300: string;
+    purple500: string;
+    purple600: string;
+    green300: string;
+    green500: string;
+    green600: string;
+    yellow300: string;
+    yellow500: string;
+    yellow600: string;
+    grey100: string;
+    grey200: string;
+    grey300: string;
+    grey400: string;
+    grey500: string;
+    black: string;
+    white: string;
+  }
+
+  interface PaletteOptions {
+    blue300?: string;
+    blue400?: string;
+    blue500?: string;
+    blue600?: string;
+    blue700?: string;
+    pink300?: string;
+    pink500?: string;
+    pink600?: string;
+    purple300?: string;
+    purple500?: string;
+    purple600?: string;
+    green300?: string;
+    green500?: string;
+    green600?: string;
+    yellow300?: string;
+    yellow500?: string;
+    yellow600?: string;
+    grey100?: string;
+    grey200?: string;
+    grey300?: string;
+    grey400?: string;
+    grey500?: string;
+    black?: string;
+    white?: string;
+  }
+}
