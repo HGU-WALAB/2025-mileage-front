4bf7a23d78bd7d0b5e5e74aac44a06d26aece1d8
Hyelim Choi <HyelimChoi01@gmail.com>
2025-02-07 20:16:31 +0900

    feat: 헤더 내 테마 모드 토클 버튼 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import useThemeStore from '@/stores/useThemeStore';
 import { globalStyle } from '@/styles/global';
 import { darkTheme, lightTheme } from '@/styles/theme';
 import { Global } from '@emotion/react';
-import { ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 
@@ -16,6 +16,7 @@ const App = () => {
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={themeMode === 'light' ? lightTheme : darkTheme}>
         <Global styles={globalStyle} />
+        <CssBaseline />
         <RouterProvider router={router} />
       </ThemeProvider>
     </QueryClientProvider>
--- src/components/_common/Header/Header.tsx
+++ src/components/_common/Header/Header.tsx
@@ -1,14 +1,15 @@
 import { Flex } from '@/components';
 import { headerHeight } from '@/constants/layoutSize';
-import { useDrawerStore } from '@/stores';
+import { useDrawerStore, useThemeStore } from '@/stores';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import { IconButton } from '@mui/material';
+import { Button, IconButton } from '@mui/material';
 
 const Header = () => {
+  const toggleTheme = useThemeStore(state => state.toggleTheme);
   const { isDrawerOpen, toggleDrawer } = useDrawerStore();
 
   return (
-    <Flex.Row height={`${headerHeight}px`} backgroundColor="yellow">
+    <Flex.Row height={`${headerHeight}px`} backgroundColor="yellow" justify="space-between">
       <Flex.Row backgroundColor="red" padding="1rem" align="center" justify="center">
         <IconButton
           color="inherit"
@@ -25,6 +26,10 @@ const Header = () => {
           <ChevronRightIcon />
         </IconButton>
       </Flex.Row>
+
+      <Button variant="contained" color="primary" size="medium" onClick={toggleTheme}>
+        테마 변경
+      </Button>
     </Flex.Row>
   );
 };
