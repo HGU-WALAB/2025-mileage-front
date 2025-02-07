60ec1133715d71bb65daec9478e7589d8708bec7
Hyelim Choi <HyelimChoi01@gmail.com>
2025-02-07 21:00:30 +0900

    feat: heading 사용하여 페이지 타이틀 추가

---
+++ src/pages/MileageListPage.tsx
@@ -0,0 +1,11 @@
+import Heading from '@/components/_common/Heading/Heading';
+
+const MileageListPage = () => {
+  return (
+    <div>
+      <Heading as={'h2'}>마일리지 조회</Heading>
+    </div>
+  );
+};
+
+export default MileageListPage;
