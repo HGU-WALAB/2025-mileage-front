0c4478194c919d032b4b76cf890b6f4b84ae9520
Hyelim Choi <HyelimChoi01@gmail.com>
2025-02-07 20:38:49 +0900

    feat: heading 컴포넌트 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ export { default as Button } from './_common/Button/Button';
 export { default as Drawer } from './_common/Drawer/Drawer';
 export { default as Flex } from './_common/Flex/Flex';
 export { default as Header } from './_common/Header/Header';
+export { default as Heading } from './_common/Heading/Heading';
 export { default as DrawerLayout } from './_common/Layout/DrawerLayout';
 export { default as Layout } from './_common/Layout/Layout';
 export { default as Main } from './_common/Main/Main';
