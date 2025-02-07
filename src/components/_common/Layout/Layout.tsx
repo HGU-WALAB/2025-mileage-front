5e2d319b067ed2d1ba6a2eead01f8f12f2538a30
Hyelim Choi <HyelimChoi01@gmail.com>
2025-02-07 17:53:32 +0900

    feat: layout 컴포넌트 추가

---
+++ src/components/_common/Layout/Layout.tsx
@@ -0,0 +1,29 @@
+import styled from '@emotion/styled';
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <S.Wrapper>
+      {/* <Header /> */}
+      <GlobalSuspense>
+        <Outlet />
+      </GlobalSuspense>
+    </S.Wrapper>
+  );
+};
+
+export default Layout;
+
+const GlobalSuspense = ({ children }: { children: JSX.Element }) => (
+  <Suspense fallback={<div style={{ height: '100vh' }}></div>}>{children}</Suspense>
+);
+
+const S = {
+  Wrapper: styled.div`
+    flex: 1;
+    min-height: 100dvh;
+
+    /* padding-top: var(--header-height); */
+  `,
+};
