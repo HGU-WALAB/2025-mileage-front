0cc596881ddb86bc5b404e8c077c3525d4235839
Hyelim Choi <HyelimChoi01@gmail.com>
2025-02-10 18:51:18 +0900

    test: msw 활용하여 마일리지 데이터 필터링 구현

--- src/mocks/handlers/mileage.ts
+++ src/mocks/handlers/mileage.ts
@@ -5,7 +5,46 @@ import { ENDPOINT } from '@/apis/endPoint';
 import { mileageList } from '@/mocks/fixtures/mileageList';
 
 export const MileageHandlers = [
-  http.get(BASE_URL + `${ENDPOINT.MILEAGE}/:studentId/search`, () => {
-    return HttpResponse.json(mileageList, { status: 200 });
+  http.get(BASE_URL + `${ENDPOINT.MILEAGE}/:studentId/search`, req => {
+    let filteredMileageList = mileageList.data;
+    const url = new URL(req.request.url);
+
+    const keyword = url.searchParams.get('keyword');
+    const categoryName = url.searchParams.get('categoryName');
+    const semester = url.searchParams.get('semester');
+    const done = url.searchParams.get('done');
+
+    if (keyword) {
+      filteredMileageList = filteredMileageList.filter(
+        mileageList =>
+          mileageList.categoryName.includes(keyword) ||
+          mileageList.capabilityName.includes(keyword) ||
+          mileageList.description.includes(keyword),
+      );
+    }
+
+    if (categoryName) {
+      filteredMileageList = filteredMileageList.filter(mileageList => {
+        mileageList.categoryName.includes(categoryName);
+      });
+    }
+
+    if (semester) {
+      filteredMileageList = filteredMileageList.filter(mileageList => {
+        if (semester === '전체') return true;
+        return mileageList.semester.includes(semester);
+      });
+    }
+
+    if (done) {
+      filteredMileageList = filteredMileageList.filter(mileageList => {
+        if (done === 'all') return true;
+        if (done === 'Y') return mileageList.done === true;
+        if (done === 'N') return mileageList.done === false;
+        return true;
+      });
+    }
+
+    return HttpResponse.json({ data: filteredMileageList }, { status: 200 });
   }),
 ];
