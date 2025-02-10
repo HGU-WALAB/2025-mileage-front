edc86f0522f550567246433827766252af2d05d1
Hyelim Choi <HyelimChoi01@gmail.com>
2025-02-10 17:26:00 +0900

    feat: input 컴포넌트 작업

--- src/components/_common/Input/Input.tsx
+++ src/components/_common/Input/Input.tsx
@@ -1,13 +1,19 @@
-import { InputVariant } from '@/types/style';
-import TextField from '@mui/material/TextField';
+import { InputVariant, Size } from '@/types/style';
+import TextField, { TextFieldProps } from '@mui/material/TextField';
 
-interface Props {
+interface Props extends Omit<TextFieldProps, 'variant'> {
   label: string;
+  size?: Exclude<Size, 'large'>;
   variant?: InputVariant;
 }
 
-const Input = ({ label, variant = 'outlined' }: Props) => {
-  return <TextField label={label} variant={variant} />;
+const Input = ({
+  label,
+  size = 'small',
+  variant = 'outlined',
+  ...props
+}: Props) => {
+  return <TextField label={label} size={size} variant={variant} {...props} />;
 };
 
 export default Input;
