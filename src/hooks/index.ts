58a39aac3643f10f7ffcc62741872d56eb42fda3
Hyelim Choi <HyelimChoi01@gmail.com>
2025-02-12 21:25:13 +0900

    feat: useInput 훅 추가

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,6 +2,7 @@ export { default as useFilteredByJoined } from './useFilteredByJoined';
 export { default as useFilteredByKeyword } from './useFilteredByKeyword';
 export { default as useFilteredBySemester } from './useFilteredBySemester';
 export { default as useGroupedMileageList } from './useGroupedMileageList';
+export { default as useInput } from './useInput';
 export { default as useInputWithValidate } from './useInputWithValidate';
 export { default as useOpenModal } from './useOpenModal';
 export { default as useQueryParams } from './useQueryParams';
---
+++ src/hooks/useInput.ts
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+const useInput = (initialValue: string = '') => {
+  const [value, setValue] = useState(initialValue);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => setValue(e.target.value);
+
+  return {
+    value,
+    handleChange,
+  };
+};
+
+export default useInput;
