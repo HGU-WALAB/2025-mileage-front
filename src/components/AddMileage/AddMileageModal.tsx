8f9a710fb3b9fc2f3a23d2fcf730d54a7894e22c
Hyelim Choi <HyelimChoi01@gmail.com>
2025-02-12 21:26:02 +0900

    feat: 등록 상세 폼 필드 작업

--- src/components/AddMileage/AddMileageModal.tsx
+++ src/components/AddMileage/AddMileageModal.tsx
@@ -1,8 +1,15 @@
-import { Button, Modal } from '@/components';
-import { useOpenModal } from '@/hooks';
+import { Button, FormField, Modal } from '@/components';
+import { useInput, useInputWithValidate, useOpenModal } from '@/hooks';
+import { validateRequired } from '@/utils/validate';
 
 const AddMileageModal = () => {
   const { open, toggleModal } = useOpenModal();
+  const {
+    value: title,
+    handleChange: handleTitle,
+    errorMessage: titleErrorMessage,
+  } = useInputWithValidate('', validateRequired);
+  const { value: desc, handleChange: handleDesc } = useInput('dd');
 
   return (
     <Modal
@@ -12,7 +19,21 @@ const AddMileageModal = () => {
       size="large"
     >
       <Modal.Header position="center">마일리지 활동 등록하기</Modal.Header>
-      <Modal.Body position="center">?</Modal.Body>
+      <Modal.Body position="center">
+        <form style={{ width: '100%' }}>
+          <FormField>
+            <FormField.Label label={'등록 상세 정보'} required />
+            <FormField.Input
+              placeholder="활동 항목에 대해 작성 해주세요"
+              fullWidth
+              value={title}
+              onChange={handleTitle}
+            />
+            <FormField.ErrorMessage value={titleErrorMessage} />
+          </FormField>
+
+        </form>
+      </Modal.Body>
       <Modal.Footer position="center">
         <Button label="닫기" onClick={toggleModal} />
       </Modal.Footer>
