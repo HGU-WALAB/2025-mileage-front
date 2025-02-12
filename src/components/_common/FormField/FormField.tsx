99e39abdbc047aa10583e90732c172cba4326ea7
Hyelim Choi <HyelimChoi01@gmail.com>
2025-02-12 20:21:57 +0900

    feat: formfield 컴포넌트 작업

---
+++ src/components/_common/FormField/FormField.tsx
@@ -0,0 +1,39 @@
+import { Asterisk } from '@/assets';
+import { Flex, Heading, Input, Text } from '@/components';
+import { GetProps } from '@/utils/getProps';
+import { styled } from '@mui/material';
+import { HTMLAttributes } from 'react';
+
+const FormFieldWrapper = styled('div')`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  width: 100%;
+`;
+
+interface LabelProps extends HTMLAttributes<HTMLDivElement> {
+  label: string;
+  required?: boolean;
+}
+
+const FormField = Object.assign(FormFieldWrapper, {
+  Label: ({ label, required = false, ...rest }: LabelProps) => {
+    return (
+      <Flex.Row gap=".25rem">
+        <Heading as="h3" {...rest}>
+          {label}
+        </Heading>
+        {required && <Asterisk />}
+      </Flex.Row>
+    );
+  },
+  Input: ({ ...rest }: GetProps<typeof Input>) => <Input {...rest} />,
+  ErrorMessage: ({
+    value,
+    ...rest
+  }: { value: string } & GetProps<typeof Text>) => (
+    <Text {...rest}>{value}</Text>
+  ),
+});
+
+export default FormField;
