2fd8c10bd7425af8e5ca87bc5706c58a4c6144f5
Hyelim Choi <HyelimChoi01@gmail.com>
2025-02-13 18:36:46 +0900

    feat: 마일리지 추가 api 작업

--- src/apis/mileage.ts
+++ src/apis/mileage.ts
@@ -1,6 +1,11 @@
 import { ENDPOINT } from '@/apis/endPoint';
 import { http } from '@/apis/http';
-import { MileageRequest, MileageResponse } from '@/types/mileage';
+import {
+  MileageRequest,
+  MileageResponse,
+  NewMileageResponse,
+} from '@/types/mileage';
+import axios, { GenericFormData } from 'axios';
 
 export const getMileageList = async ({
   studentId,
@@ -24,15 +29,36 @@ export const getMileageList = async ({
   return response.data;
 };
 
-export const getEtcMileageList = async ({ semester = 'all' }) => {
-  const queryParams = new URLSearchParams({
-    semester,
-  });
-
+export const getEtcMileageList = async () => {
   const response = await http.get<{ data: MileageResponse[] }>(
     `${ENDPOINT.NEW_MILEAGE}`,
-    { params: queryParams },
   );
 
   return response.data;
+};
+
+export const postNewMileage = async ({
+  semester,
+  description1,
+  description2,
+  file,
+}: NewMileageResponse) => {
+  const data = axios.toFormData({
+    semester,
+    description1,
+    description2,
+    file,
+  });
+
+  const response = await http.post<GenericFormData>(
+    `${ENDPOINT.NEW_MILEAGE}`,
+    data,
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    },
+  );
+
+  return response;
 };
