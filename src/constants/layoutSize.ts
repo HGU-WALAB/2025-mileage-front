9c28c2de0229db6508f3d54cdce3884ee6de9d6b
Hyelim Choi <HyelimChoi01@gmail.com>
2025-02-16 15:02:58 +0900

    chore: 컴포넌트 사이즈 수정

--- src/constants/layoutSize.ts
+++ src/constants/layoutSize.ts
@@ -1,2 +1,3 @@
-export const drawerWidth = 240;
-export const headerHeight = 50;
+export const drawerWidth = 200;
+export const headerHeight = 60;
+export const globalHeight = `calc(100dvh - 2rem)`;
