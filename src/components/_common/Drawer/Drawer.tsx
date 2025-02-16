03146a64e6170067c04c8b23f3d0ac47b76425d6
Hyelim Choi <HyelimChoi01@gmail.com>
2025-02-16 15:01:43 +0900

    style: drawer gui 픽스

--- src/components/_common/Drawer/Drawer.tsx
+++ src/components/_common/Drawer/Drawer.tsx
@@ -1,6 +1,4 @@
 import {
-  Divider,
-  IconButton,
   List,
   ListItem,
   ListItemButton,
@@ -10,17 +8,18 @@ import {
   useTheme,
 } from '@mui/material';
 
-import { LeftArrowIcon } from '@/assets';
-import { Flex } from '@/components';
+import { Flex, Heading } from '@/components';
+import { UserBox } from '@/components/User';
 import drawerItems from '@/constants/drawerItems';
-import { drawerWidth, headerHeight } from '@/constants/layoutSize';
+import { drawerWidth, globalHeight } from '@/constants/layoutSize';
 import { useDrawerStore } from '@/stores';
+import { getOpacityColor } from '@/utils/getOpacityColor';
 import { Link, useLocation } from 'react-router-dom';
 
 const Drawer = () => {
   const query = useLocation();
   const theme = useTheme();
-  const { isDrawerOpen, toggleDrawer } = useDrawerStore();
+  const { isDrawerOpen } = useDrawerStore();
 
   return (
     <>
@@ -29,47 +28,81 @@ const Drawer = () => {
           width: drawerWidth,
           flexShrink: 0,
           '& .MuiDrawer-paper': {
+            top: '1rem',
+            left: '1rem',
+            height: globalHeight,
             width: drawerWidth,
             boxSizing: 'border-box',
-            backgroundColor: theme.palette.variant.default,
+
+            backdropFilter: `blur(10px)`,
+            backgroundColor: getOpacityColor(theme.palette.white, 0.1),
+            borderRadius: '.5rem',
             boxShadow:
               theme.palette.mode === 'light'
-                ? '0px 4px 6px rgba(0, 0, 0, 0.1)'
-                : '0px 4px 10px rgba(0, 0, 0, 0.5)',
+                ? `0px 4px 6px ${getOpacityColor(theme.palette.black, 0.1)}`
+                : `0px 4px 10px ${getOpacityColor(theme.palette.black, 0.5)}`,
           },
         }}
         variant="persistent"
         anchor="left"
         open={isDrawerOpen}
       >
-        <Flex
-          direction="row-reverse"
-          height={`${headerHeight}px`}
-          padding="1rem"
-        >
-          <IconButton onClick={toggleDrawer}>
-            <LeftArrowIcon />
-          </IconButton>
-        </Flex>
-        <Divider />
-        <List>
-          {drawerItems.map(item => (
-            <Link to={item.route} key={item.text}>
-              <ListItem disablePadding>
-                <ListItemButton selected={query.pathname === item.route}>
-                  <ListItemIcon>
-                    <item.icon />
-                  </ListItemIcon>
-                  <ListItemText
-                    primary={item.text}
-                    sx={{ fontSize: theme.typography.body1 }}
-                  />
-                </ListItemButton>
-              </ListItem>
-              <Divider />
-            </Link>
-          ))}
-        </List>
+        <Flex.Column align="center" padding="1rem" gap="1rem">
+          <Heading as="h2" color={theme.palette.white}>
+            CSEE MILEAGE
+          </Heading>
+
+          <UserBox />
+
+          <List
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '.5rem',
+              width: '100%',
+            }}
+          >
+            {drawerItems.map(item => (
+              <Link to={item.route} key={item.text}>
+                <ListItem disablePadding style={{ color: theme.palette.white }}>
+                  <ListItemButton
+                    selected={query.pathname === item.route}
+                    sx={{
+                      borderRadius: '.5rem',
+                      '&:hover': {
+                        backgroundColor: getOpacityColor(
+                          theme.palette.white,
+                          0.2,
+                        ),
+                      },
+                      '&.Mui-selected': {
+                        fontWeight: 'bold',
+                        backgroundColor: getOpacityColor(
+                          theme.palette.white,
+                          0.2,
+                        ),
+                        '&:hover': {
+                          backgroundColor: getOpacityColor(
+                            theme.palette.white,
+                            0.4,
+                          ),
+                        },
+                      },
+                    }}
+                  >
+                    <ListItemIcon style={{ minWidth: '2.5rem' }}>
+                      <item.icon style={{ color: theme.palette.white }} />
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={item.text}
+                      sx={{ fontSize: theme.typography.body1 }}
+                    />
+                  </ListItemButton>
+                </ListItem>
+              </Link>
+            ))}
+          </List>
+        </Flex.Column>
       </MuiDrawer>
     </>
   );
