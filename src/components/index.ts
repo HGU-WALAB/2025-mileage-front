02e245e67a0c3da90d78ef24f2f7f01a355ef343
Hyelim Choi <HyelimChoi01@gmail.com>
2025-02-16 15:02:04 +0900

    style: 변경된 레이아웃 디자인 반영

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,16 @@
 export { default as Button } from './_common/Button/Button';
 export { default as Drawer } from './_common/Drawer/Drawer';
 export { default as Dropdown } from './_common/Dropdown/Dropdown';
+export { default as ErrorFallbackSection } from './_common/Error/ErrorFallbackSection';
+export { default as ErrorResetBoundary } from './_common/Error/ErrorResetBoundary';
 export { default as GlobalErrorBoundary } from './_common/Error/GlobalErrorBoundary';
+export { default as GlobalSuspense } from './_common/Error/GlobalSuspense';
 export { default as Flex } from './_common/Flex/Flex';
 export { default as FormField } from './_common/FormField/FormField';
 export { default as Header } from './_common/Header/Header';
 export { default as Heading } from './_common/Heading/Heading';
 export { default as Input } from './_common/Input/Input';
+export { default as AuthGuard } from './_common/Layout/AuthGuard';
 export { default as DrawerLayout } from './_common/Layout/DrawerLayout';
 export { default as Layout } from './_common/Layout/Layout';
 export { default as Main } from './_common/Main/Main';
