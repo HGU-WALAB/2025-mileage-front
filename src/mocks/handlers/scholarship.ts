1595431ebd254bc96566455f75525b1be1f8b0f0
Hyelim Choi <HyelimChoi01@gmail.com>
2025-02-17 15:19:07 +0900

    feat: 400 에러 발생 유틸 작업

--- src/mocks/handlers/scholarship.ts
+++ src/mocks/handlers/scholarship.ts
@@ -2,9 +2,17 @@ import { http, HttpResponse } from 'msw';
 
 import { BASE_URL } from '@/apis/config';
 import { ENDPOINT } from '@/apis/endPoint';
+import { Error400, Error401, Error500, randomMswError } from '@/utils/mswError';
 
 export const ScholarshipHandlers = [
   http.post(BASE_URL + `${ENDPOINT.SCHOLARSHIP_APPLY}/:studentId`, () => {
+    const { is400Error, is401Error, is500Error } = randomMswError();
+
+    if (is400Error)
+      return Error400('이미 신청된 학생이거나 존재하지 않는 학생입니다.');
+    if (is401Error) return Error401();
+    if (is500Error) return Error500();
+
     return HttpResponse.json({}, { status: 200 });
   }),
 ];
--- src/utils/mswError.ts
+++ src/utils/mswError.ts
@@ -1,7 +1,8 @@
 import { HttpResponse } from 'msw';
 
-const ERROR_401_CHANCE = 0.33;
-const ERROR_500_CHANCE = 0.33;
+const ERROR_400_CHANCE = 0.1;
+const ERROR_401_CHANCE = 0.1;
+const ERROR_500_CHANCE = 0.1;
 
 const isErrorRandomly = (threshold: number) => {
   const randomNumber = Math.random();
@@ -9,22 +10,45 @@ const isErrorRandomly = (threshold: number) => {
 };
 
 export const randomMswError = () => {
+  const is400Error = isErrorRandomly(ERROR_400_CHANCE);
   const is401Error = isErrorRandomly(ERROR_401_CHANCE);
   const is500Error = isErrorRandomly(ERROR_500_CHANCE);
 
-  return { is401Error, is500Error };
+  return { is400Error, is401Error, is500Error };
 };
 
-export const Error401 = () => {
-  return new HttpResponse(null, {
-    status: 401,
-    statusText: 'Authorization Error',
-  });
+export const Error400 = (message: string = '400 ERROR') => {
+  return HttpResponse.json(
+    {
+      message,
+    },
+    {
+      status: 400,
+      statusText: 'Bad Request Error',
+    },
+  );
 };
 
-export const Error500 = () => {
-  return new HttpResponse(null, {
-    status: 500,
-    statusText: 'Internal Server Error',
-  });
+export const Error401 = (message: string = '401 ERROR') => {
+  return HttpResponse.json(
+    {
+      message,
+    },
+    {
+      status: 401,
+      statusText: 'Authorization Error',
+    },
+  );
+};
+
+export const Error500 = (message: string = '500 ERROR') => {
+  return HttpResponse.json(
+    {
+      message,
+    },
+    {
+      status: 500,
+      statusText: 'Internal Server Error',
+    },
+  );
 };
