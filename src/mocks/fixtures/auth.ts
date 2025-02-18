4c30fd45720e216e5cb66c4dc10a118d395edc3e
Hyelim Choi <HyelimChoi01@gmail.com>
2025-02-19 00:19:21 +0900

    test: 로그인 로직 msw 세팅

---
+++ src/mocks/fixtures/auth.ts
@@ -0,0 +1,12 @@
+import { UserResponse } from '@/types/auth';
+
+export const mockUserData: UserResponse = {
+  studentId: '22000770',
+  studentName: '김철수',
+  studentEmail: 'chulsoo.kim@example.com',
+  major1: '컴퓨터공학',
+  major2: '경영학',
+  grade: 3,
+  term: 1,
+  department: '공과대학',
+};
