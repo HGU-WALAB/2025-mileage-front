f32d43851ea96daed323cad323302be190313d75
Hyelim Choi <HyelimChoi01@gmail.com>
2025-02-19 00:18:52 +0900

    feat: 로그인 api 로직 작업

--- src/apis/endPoint.ts
+++ src/apis/endPoint.ts
@@ -2,5 +2,8 @@ export const ENDPOINT = {
   // mileage
   MILEAGE: `/api/mileage`,
   ETC_MILEAGE: `/api/mileage/etc`,
+  // scholarship
   SCHOLARSHIP_APPLY: `/api/mileage/apply`,
+  // auth
+  AUTH: `/api/mileage/auth/login`,
 };
---
+++ src/types/auth.ts
@@ -0,0 +1,15 @@
+export interface AuthRequest {
+  accessKey: string;
+  token: string;
+}
+
+export interface UserResponse {
+  studentId: string;
+  studentName: string;
+  studentEmail: string;
+  major1: string;
+  major2: string;
+  grade: number;
+  term: number;
+  department: string;
+}
