a3334963d85fc2294938763c9ec4a95eac19e601
Hyelim Choi <HyelimChoi01@gmail.com>
2025-02-24 21:06:21 +0900

    feat: 로그인 페이지 사진 크루셀 기능 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,4 @@
+// icons
 import AsteriskIcon from '@/assets/icons/asterisk.svg?react';
 import CheckIcon from '@/assets/icons/check.svg?react';
 import CheckCircleIcon from '@/assets/icons/checkCircle.svg?react';
@@ -8,9 +9,17 @@ import MileageAddIcon from '@/assets/icons/mileageAdd.svg?react';
 import MileageListIcon from '@/assets/icons/mileageList.svg?react';
 import ScholarshipIcon from '@/assets/icons/scholarship.svg?react';
 import SearchIcon from '@/assets/icons/search.svg?react';
+// imgs
+import Img1 from '@/assets/imgs/1.png';
+import Img2 from '@/assets/imgs/2.png';
+import Img3 from '@/assets/imgs/3.png';
+import Img4 from '@/assets/imgs/4.png';
+import Img5 from '@/assets/imgs/5.png';
+import Img6 from '@/assets/imgs/6.png';
+import Img7 from '@/assets/imgs/7.png';
 import BackgroundImg from '@/assets/imgs/background.png';
 import EmptyBoxImg from '@/assets/imgs/emptyBox.svg?react';
-import MoneyImg from '@/assets/imgs/money.svg?react';
+// mui
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import CloseIcon from '@mui/icons-material/Close';
@@ -26,12 +35,18 @@ export {
   CloseIcon,
   CloudUploadIcon,
   EmptyBoxImg,
+  Img1,
+  Img2,
+  Img3,
+  Img4,
+  Img5,
+  Img6,
+  Img7,
   ChevronLeftIcon as LeftArrowIcon,
   LogoIcon,
   LogoutIcon,
   MileageAddIcon,
   MileageListIcon,
-  MoneyImg,
   ChevronRightIcon as RightArrowIcon,
   ScholarshipIcon,
   SearchIcon,
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,10 +1,23 @@
 import { Flex } from '@/components';
 import { LoginSection, LogoSection, NoticeCard } from '@/components/Login';
+import { ROUTE_PATH } from '@/constants/routePath';
+import { useAuthStore } from '@/stores';
 import { boxShadow } from '@/styles/common';
 import { getOpacityColor } from '@/utils/getOpacityColor';
 import { styled } from '@mui/material';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const { isLogin } = useAuthStore();
+
+  useEffect(() => {
+    if (isLogin) {
+      navigate(ROUTE_PATH.dashboard);
+    }
+  }, [isLogin, navigate]);
+
   return (
     <Flex.Column justify="center" align="center" height="100%">
       <S.GlassContainer
@@ -20,7 +33,7 @@ const LoginPage = () => {
           height="50%"
           gap="2rem"
         >
-          <Flex.Column width="70%">
+          <Flex.Column gap="1rem">
             <LogoSection />
             <NoticeCard />
           </Flex.Column>
@@ -38,7 +51,7 @@ const S = {
   GlassContainer: styled(Flex)`
     backdrop-filter: blur(1.875rem);
     background-color: ${({ theme }) =>
-      getOpacityColor(theme.palette.white, 0.1)};
+      getOpacityColor(theme.palette.white, 0.2)};
     border-radius: 1rem;
     ${boxShadow}
   `,
