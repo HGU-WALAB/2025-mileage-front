8d568e019ccd22d490e3b2e2a5ae68e296533094
Hyelim Choi <HyelimChoi01@gmail.com>
2025-02-24 21:02:14 +0900

    chore: 타이포 사이즈 변경

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -1,32 +1,32 @@
 export const typography = {
   fontFamily: '"Roboto", "Arial", sans-serif',
   h1: {
-    fontSize: '1.75rem',
-    fontWeight: 700,
-    lineHeight: 1.5,
-  },
-  h2: {
     fontSize: '1.5rem',
     fontWeight: 700,
     lineHeight: 1.5,
   },
-  h3: {
+  h2: {
     fontSize: '1.25rem',
+    fontWeight: 700,
+    lineHeight: 1.5,
+  },
+  h3: {
+    fontSize: '1.125rem',
     fontWeight: 600,
     lineHeight: 1.4,
   },
   h4: {
-    fontSize: '1.125rem',
+    fontSize: '1rem',
     fontWeight: 600,
     lineHeight: 1.6,
   },
   h5: {
-    fontSize: '1rem',
+    fontSize: '.875rem',
     fontWeight: 500,
     lineHeight: 1.5,
   },
   h6: {
-    fontSize: '0.875rem',
+    fontSize: '0.75rem',
     fontWeight: 500,
     lineHeight: 1.5,
   },
