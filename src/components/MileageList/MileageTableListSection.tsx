a6f2ae589130dfa1d2ba172c486d337a60cb8643
Hyelim Choi <HyelimChoi01@gmail.com>
2025-02-24 15:57:09 +0900

    chore: 마일리지 데이터 없을때 처리

--- src/components/MileageList/MileageTableListSection.tsx
+++ src/components/MileageList/MileageTableListSection.tsx
@@ -1,11 +1,14 @@
 import { Flex, TableListSkeleton, Title } from '@/components';
 import { MileageTable } from '@/components/MileageList';
+import EmptyMileageTable from '@/components/MileageList/EmptyMileageTable';
 import { useGroupedMileageList } from '@/hooks';
 
 const MileageTableListSection = () => {
   const { groupedMileageList, isLoading } = useGroupedMileageList();
 
   if (isLoading) return <TableListSkeleton />;
+
+  if (!groupedMileageList.length) return <EmptyMileageTable />;
 
   return (
     <>
