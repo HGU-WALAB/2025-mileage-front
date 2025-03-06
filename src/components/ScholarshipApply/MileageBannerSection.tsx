df3274fd61095999035c82b7fa3428cbe0841371
Hyelim Choi <HyelimChoi01@gmail.com>
2025-03-06 10:23:04 +0900

    feat: 장학금 설명 배너 섹션 반응형 작업

--- src/components/ScholarshipApply/MileageBannerSection.tsx
+++ src/components/ScholarshipApply/MileageBannerSection.tsx
@@ -1,20 +1,21 @@
 import { CountBoxFallback, Flex, Text } from '@/components';
 import MileageCountBox from '@/components/ScholarshipApply/MileageCountBox';
+import { MAX_RESPONSIVE_WIDTH } from '@/constants/system';
 import { useAuthStore } from '@/stores';
-import { boxShadow } from '@/styles/common';
-import { styled } from '@mui/material';
+import { styled, useMediaQuery } from '@mui/material';
 import { ErrorBoundary } from 'react-error-boundary';
 
 const MileageBannerSection = () => {
+  const isMobile = useMediaQuery(MAX_RESPONSIVE_WIDTH);
   const { student, currentSemester } = useAuthStore();
 
   return (
-    <S.Wrapper justify="center" align="center">
-      <S.Container justify="center" padding="3rem">
-        <Text as="h2" style={{ fontSize: '1.25rem' }}>
+    <S.Wrapper justify="center" align="center" padding="1rem">
+      <S.Container justify="center" isMobile={isMobile}>
+        <Text as="h2" style={{ fontSize: isMobile ? '16px' : '24px' }}>
           현재 {student?.studentName} 학부생님의
         </Text>
-        <Text as="h2" style={{ fontSize: '1.25rem' }}>
+        <Text as="h2" style={{ fontSize: isMobile ? '16px' : '24px' }}>
           <span style={{ fontWeight: 'bold' }}>{currentSemester} 학기 </span>
           참여하신 마일리지입니다
         </Text>
@@ -33,31 +34,14 @@ const S = {
     background-color: ${({ theme }) => theme.palette.primary.light};
     color: ${({ theme }) => theme.palette.white};
     height: 200px;
-    padding: 2rem;
   `,
-  Container: styled(Flex.Column)`
+  Container: styled(Flex.Column)<{ isMobile: boolean }>`
     background-color: ${({ theme }) => theme.palette.primary.main};
     border-radius: 1rem;
     color: ${({ theme }) => theme.palette.white};
     height: 110px;
+    padding: ${({ isMobile }) => (isMobile ? '1rem' : '3rem')};
     position: relative;
-    width: 80%;
-  `,
-  CountContainer: styled(Flex.Column)`
-    background-color: ${({ theme }) => theme.palette.white};
-    border-radius: 1rem;
-    color: ${({ theme }) => theme.palette.black};
-    height: 110px;
-    padding: 1rem;
-    position: absolute;
-    right: 10%;
-    top: -30%;
-    width: 20%;
-    ${boxShadow}
-  `,
-  CountNumber: styled(Flex.Row)`
-    color: ${({ theme }) => theme.palette.primary.main};
-    font-size: 2.5rem;
-    font-weight: bold;
+    width: ${({ isMobile }) => (isMobile ? '100%' : '80%')};
   `,
 };
