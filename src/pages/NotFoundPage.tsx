e75de39ec096835c14aa03702ef28cb2d944ca59
Hyelim Choi <HyelimChoi01@gmail.com>
2025-03-12 18:03:55 +0900

    refactor: not found 페이지 작업

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,27 @@
+import { LogoIcon } from '@/assets';
+import { Flex, Heading } from '@/components';
+import { getErrorMessage } from '@/utils/getErrorMessage';
+import { useTheme } from '@mui/material';
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  const theme = useTheme();
+  return (
+    <Flex.Column
+      width="100%"
+      height="600px"
+      gap="2rem"
+      justify="center"
+      align="center"
+    >
+      <LogoIcon width={100} height={100} />
+      <Heading
+        as="h2"
+        style={{ fontSize: '1.5rem', color: theme.palette.grey400 }}
+      >
+        {getErrorMessage('404')}
+      </Heading>
+    </Flex.Column>
+  );
 };
 
 export default NotFoundPage;
