1e016a4519ca19682eae7aa5e901a5296e82df14
Hyelim Choi <HyelimChoi01@gmail.com>
2025-03-14 01:21:42 +0900

    chore: 장학금 신청 기간 아님 텍스트 색상 변경

--- src/components/ScholarshipApply/NotScholarshipDurationSection.tsx
+++ src/components/ScholarshipApply/NotScholarshipDurationSection.tsx
@@ -1,6 +1,7 @@
 import { EmptyBoxImg } from '@/assets';
 import { Flex, Heading } from '@/components';
 import MileageBannerSection from '@/components/ScholarshipApply/MileageBannerSection';
+import { styled } from '@mui/material';
 
 const NotScholarshipDurationSection = () => {
   return (
@@ -14,13 +15,18 @@ const NotScholarshipDurationSection = () => {
         gap="1rem"
       >
         <EmptyBoxImg width={100} height={100} />
-        <Heading as="h2">현재 마일리지 장학금 신청기간이 아닙니다.</Heading>
-        <Heading as="h2">
-          신청 기한 내에 마일리지 장학금을 신청해주세요.
-        </Heading>
+        <S.Text as="h3">현재 마일리지 장학금 신청기간이 아닙니다.</S.Text>
+        <S.Text as="h3">신청 기한 내에 마일리지 장학금을 신청해주세요.</S.Text>
       </Flex.Column>
     </Flex.Column>
   );
 };
 
 export default NotScholarshipDurationSection;
+
+const S = {
+  Text: styled(Heading)`
+    ${({ theme }) => theme.typography.h3}
+    color: ${({ theme }) => theme.palette.grey500};
+  `,
+};
