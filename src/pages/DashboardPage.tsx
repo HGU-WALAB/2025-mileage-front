2c845da267c2731c612a6bc0905fde15d2ca4df9
Hyelim Choi <HyelimChoi01@gmail.com>
2025-03-14 11:42:43 +0900

    fix: 장학금 신청 기간 컴포넌트 모바일에서 숨기기

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -6,15 +6,19 @@ import {
   ProcessSection,
   ScholarshipDurationSection,
 } from '@/components/Dashboard';
+import { MAX_RESPONSIVE_WIDTH } from '@/constants/system';
 import { useTrackPageView } from '@/service/amplitude/useTrackPageView';
+import { useMediaQuery } from '@mui/material';
 
 const DashboardPage = () => {
   useTrackPageView({ eventName: '[View] 대시보드 페이지' });
 
+  const isMobile = useMediaQuery(MAX_RESPONSIVE_WIDTH);
+
   return (
     <Flex.Column margin="1rem 1rem 2rem" gap="1rem">
       <Flex.Row justify="space-between" align="flex-end">
-        <ScholarshipDurationSection />
+        {!isMobile && <ScholarshipDurationSection />}
         <MileageCountSection />
       </Flex.Row>
 
