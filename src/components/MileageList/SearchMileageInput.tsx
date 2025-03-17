d30faa79254e97d0ce5a34cd0f1d6d396c70d3c7
Hyelim Choi <HyelimChoi01@gmail.com>
2025-03-17 12:26:15 +0900

    feat: 성능최적화를 위한 인풋 디바운스 적용

--- src/components/MileageList/SearchMileageInput.tsx
+++ src/components/MileageList/SearchMileageInput.tsx
@@ -2,23 +2,31 @@ import { SearchIcon } from '@/assets';
 import { Flex, Input } from '@/components';
 import { useFilteredByKeyword } from '@/hooks';
 import { styled, useTheme } from '@mui/material';
+import { useState } from 'react';
 
 const SearchMileageInput = () => {
   const theme = useTheme();
   const { keyword, setKeyword } = useFilteredByKeyword();
+  const [localKeyword, setLocalKeyword] = useState(keyword);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newKeyword = e.target.value;
+    setLocalKeyword(newKeyword);
+    setKeyword(newKeyword);
+  };
 
   return (
     <Flex.Row gap=".5rem">
       <Input
         placeholder="항목명을 입력하세요"
-        value={keyword}
-        onChange={e => setKeyword(e.target.value)}
+        value={localKeyword}
+        onChange={handleInputChange}
         style={{
           width: '300px',
           backgroundColor: theme.palette.variant.default,
         }}
       />
-      <S.SearchButton onClick={() => setKeyword(keyword ?? '')}>
+      <S.SearchButton onClick={() => setKeyword(localKeyword ?? '')}>
         <SearchIcon />
       </S.SearchButton>
     </Flex.Row>
--- src/hooks/useFilteredByKeyword.ts
+++ src/hooks/useFilteredByKeyword.ts
@@ -1,12 +1,17 @@
 import { useQueryParams } from '@/hooks';
+import { debounce } from '@/utils/debounce';
+import { useCallback } from 'react';
 
 const useFilteredByKeyword = () => {
   const { queryParams, updateQueryParams } = useQueryParams();
   const keyword = queryParams.keyword;
 
-  const setKeyword = (newKeyword: string) => {
-    updateQueryParams({ keyword: newKeyword });
-  };
+  const setKeyword = useCallback(
+    debounce((newKeyword: string) => {
+      updateQueryParams({ keyword: newKeyword });
+    }, 300),
+    [],
+  );
 
   return { keyword, setKeyword };
 };
