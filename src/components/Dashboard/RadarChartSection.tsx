b4afa0ed90ebcffbe60db86aa593f8c46b6df71f
Hyelim Choi <HyelimChoi01@gmail.com>
2025-05-08 17:39:49 +0900

    chore: useGetUserInfoQuery 호출 방식 변경

--- src/components/Dashboard/RadarChartSection.tsx
+++ src/components/Dashboard/RadarChartSection.tsx
@@ -8,16 +8,16 @@ export const RadarChartSection = ({
 }: {
   compareOption: string[];
 }) => {
-  const { data: user } = useGetUserInfoQuery();
+  const { userInfo } = useGetUserInfoQuery();
 
   const { capability } = useGetCapabilityQuery();
   const { compareCapability } = useGetCompareCapabilityQuery({
-    term: compareOption.includes('term') ? `${user?.term}` : undefined,
+    term: compareOption.includes('term') ? `${userInfo?.term}` : undefined,
     entryYear: compareOption.includes('entryYear')
-      ? user?.studentId.slice(1, 3)
+      ? userInfo?.studentId.slice(1, 3)
       : undefined,
-    major1: compareOption.includes('major1') ? user?.major1 : undefined,
-    major2: compareOption.includes('major2') ? user?.major2 : undefined,
+    major1: compareOption.includes('major1') ? userInfo?.major1 : undefined,
+    major2: compareOption.includes('major2') ? userInfo?.major2 : undefined,
   });
 
   const capabilityData: RadarCapability[] = (capability ?? []).map(cap => {
