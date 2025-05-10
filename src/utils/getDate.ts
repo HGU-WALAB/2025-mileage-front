9421f162549b041d43cc74dad987efb9df71906d
Hyelim Choi <HyelimChoi01@gmail.com>
2025-05-11 02:34:47 +0900

    feat: getFormattedDateFullYear utils 생성

--- src/utils/getDate.ts
+++ src/utils/getDate.ts
@@ -16,3 +16,13 @@ export const getFormattedDate = (stringDate: string) => {
 
   return `${month}월 ${day}일`;
 };
+
+export const getFormattedDateFullYear = (stringDate: string) => {
+  const date = new Date(stringDate);
+
+  const year = ('0' + date.getFullYear()).slice(-4);
+  const month = ('0' + (date.getMonth() + 1)).slice(-2);
+  const day = ('0' + date.getDate()).slice(-2);
+
+  return `${year}년 ${month}월 ${day}일`;
+};
