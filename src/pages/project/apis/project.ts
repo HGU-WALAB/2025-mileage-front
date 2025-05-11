642f61b5e3aa53e97c486c43cd74c96ef0da4abe
Hyelim Choi <HyelimChoi01@gmail.com>
2025-05-11 19:49:12 +0900

    feat: 프로젝트 추가 api 및 훅 작업

--- src/pages/project/apis/project.ts
+++ src/pages/project/apis/project.ts
@@ -1,7 +1,9 @@
 import { ENDPOINT } from '@/apis/endPoint';
 import { http } from '@/apis/http';
+import { toFormData } from '@/utils/toFormData';
+import { GenericFormData } from 'axios';
 
-import { ProjectResponse } from '../types/project';
+import { PostProjectRequest, ProjectResponse } from '../types/project';
 
 export const getProject = async ({ projectId }: { projectId: string }) => {
   const response = await http.get<ProjectResponse>(
@@ -12,5 +14,24 @@ export const getProject = async ({ projectId }: { projectId: string }) => {
 
 export const getProjectList = async () => {
   const response = await http.get<ProjectResponse[]>(`${ENDPOINT.PROJECT}`);
+  return response;
+};
+
+export const postProject = async ({
+  studentId,
+  formValues,
+}: PostProjectRequest) => {
+  const data = toFormData(formValues);
+
+  const response = await http.post<GenericFormData>(
+    `${ENDPOINT.PROJECT}/${studentId}`,
+    data,
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    },
+  );
+
   return response;
 };
