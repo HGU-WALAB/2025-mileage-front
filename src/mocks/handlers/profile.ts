c63d2a58156648de7dc91c6ba1a1a83f81db35f3
Hyelim Choi <HyelimChoi01@gmail.com>
2025-05-18 13:14:06 +0900

    fix: msw livestorage update 오류 수정

--- src/mocks/handlers/profile.ts
+++ src/mocks/handlers/profile.ts
@@ -1,20 +1,15 @@
 import { BASE_URL } from '@/apis/config';
 import { ENDPOINT } from '@/apis/endPoint';
 import { mockProfile } from '@/mocks/fixtures/profile';
-import { PatchProfileRequest } from '@/pages/profile/types/profile';
+import { ProfileResponse } from '@/pages/profile/types/profile';
 import { Error400, Error401, Error500, randomMswError } from '@/utils/mswError';
 import { LiveStorage } from '@mswjs/storage';
 import { http, HttpResponse } from 'msw';
 
 const profileStorage = new LiveStorage('profileData', mockProfile);
-const profileStorageForPatch = new LiveStorage<PatchProfileRequest>('profilePatchData', {
-  formValues: {
-    profile_image_url: new File([], ''),
-  },
-});
 
 export const ProfileHandlers = [
-  http.get(BASE_URL + ENDPOINT.PROFILE, () => {
+  http.get(BASE_URL + `${ENDPOINT.PROFILE}`, () => {
     const { is400Error, is401Error, is500Error } = randomMswError();
     if (is400Error) return Error400();
     if (is401Error) return Error401();
@@ -23,32 +18,32 @@ export const ProfileHandlers = [
     return HttpResponse.json(profileStorage.getValue(), { status: 200 });
   }),
 
-  http.patch(
-      BASE_URL + `${ENDPOINT.PROFILE}`,
-      async ({ request }) => {
-        const formData = await request.formData();
+  http.patch(BASE_URL + `${ENDPOINT.PROFILE}`, async ({ request }) => {
+    const formData = await request.formData();
 
-        const profile_image_url = formData.get('profile_image_url') as File;
-        const job = formData.get('job') as string;
-        const self_description = formData.get('self_description') as string;
-        const github_link = formData.get('github_link') as string;
-        const blog_link = formData.get('blog_link') as string;
-        const linkedin_link = formData.get('linkedin_link') as string;
-        const instagram_link = formData.get('instagram_link') as string;
+    const profile_image_url = formData.get('profile_image_url') as File;
+    const job = formData.get('job') as string;
+    const self_description = formData.get('self_description') as string;
+    const github_link = formData.get('github_link') as string;
+    const blog_link = formData.get('blog_link') as string;
+    const linkedin_link = formData.get('linkedin_link') as string;
+    const instagram_link = formData.get('instagram_link') as string;
 
-        profileStorageForPatch.update(() => ({
-            formValues: {
-                profile_image_url,
-                job,
-                self_description,
-                github_link,
-                blog_link,
-                linkedin_link,
-                instagram_link,
-            }
-        }));
-  
-        return HttpResponse.json(profileStorageForPatch.getValue(), { status: 201 });
-      },
-    ),
+    profileStorage.update(
+      () =>
+        ({
+          job: job?.toString(),
+          self_description: self_description?.toString(),
+          github_link: github_link?.toString(),
+          blog_link: blog_link?.toString(),
+          linkedin_link: linkedin_link?.toString(),
+          instagram_link: instagram_link?.toString(),
+          profile_image_url: profile_image_url?.toString(),
+        }) as ProfileResponse,
+    );
+
+    return HttpResponse.json(profileStorage.getValue(), {
+      status: 201,
+    });
+  }),
 ];
