476eb82f81ab9fcd6a6789e8340f8a34d3877e91
Yoomin Hwang <yoominh3@gmail.com>
2025-05-19 18:26:23 +0900

    feat: patch profile api 설정

--- src/pages/profile/types/profile.ts
+++ src/pages/profile/types/profile.ts
@@ -16,3 +16,15 @@ export interface ProfileResponse {
 export interface TechStack {
   techStack: string[];
 }
+
+export interface PatchSubmittedProfileRequest {
+  job?: string;
+  self_description?: string;
+
+  profile_image_url?: File;
+
+  github_link?: string;
+  blog_link?: string;
+  linkedin_link?: string;
+  instagram_link?: string;
+}
