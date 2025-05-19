d362d6231f3bf5695eff88b4ae6f60e3493dea14
Hyelim Choi <HyelimChoi01@gmail.com>
2025-05-19 16:58:08 +0900

    feat: archive dashboard suspense 처리

---
+++ src/pages/dashboard/ArchiveDashboardPage/components/AwardArchiveGrid.tsx
@@ -0,0 +1,43 @@
+import { MAX_RESPONSIVE_WIDTH } from '@/constants/system';
+import { styled, useMediaQuery } from '@mui/material';
+
+import { useGroupedAwardList } from '@award/hooks/useGroupedAwardList';
+
+import { AwardCountBox } from './AwardCountBox';
+import { AwardPageForwardButton } from './AwardPageForwardButton';
+
+export const AwardArchiveGrid = () => {
+  const isMobile = useMediaQuery(MAX_RESPONSIVE_WIDTH);
+  const { groupedAwardList } = useGroupedAwardList();
+
+  return (
+    <S.GridLayout isMobile={isMobile}>
+      {groupedAwardList.map(group => (
+        <AwardCountBox
+          key={group.awardType}
+          awardType={group.awardType}
+          length={group.items.length}
+        />
+      ))}
+
+      <AwardPageForwardButton />
+    </S.GridLayout>
+  );
+};
+
+const S = {
+  GridLayout: styled('div')<{ isMobile: boolean }>`
+    display: grid;
+    gap: 1rem;
+    grid-template-columns: ${({ isMobile }) =>
+      isMobile ? 'repeat(2, 1fr)' : 'repeat(3, 1fr)'};
+
+    ${({ isMobile }) =>
+      isMobile &&
+      `
+        & > *:last-of-type {
+          grid-column: span 2;
+        }
+      `}
+  `,
+};
