42536aa9b83b088d00e5f2b960a33a5c6d84916e
Hyelim Choi <HyelimChoi01@gmail.com>
2025-05-22 14:09:15 +0900

    chore: error page 리디자인

--- src/pages/etc/ErrorPage.tsx
+++ src/pages/etc/ErrorPage.tsx
@@ -1,29 +1,39 @@
 import { EmptyBoxImg } from '@/assets';
-import { Flex, Heading } from '@/components';
+import { Button, Flex, Heading, Main } from '@/components';
 import { ERROR_MESSAGES } from '@/constants/errorMessage';
+import { ROUTE_PATH } from '@/constants/routePath';
 import { useTrackPageView } from '@/service/amplitude/useTrackPageView';
 import { useTheme } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const ErrorPage = () => {
+  const navigate = useNavigate();
   useTrackPageView({ eventName: '[View] 에러 페이지' });
 
   const theme = useTheme();
   return (
-    <Flex.Column
-      width="100%"
-      height="400px"
-      gap="1rem"
-      justify="center"
-      align="center"
-    >
-      <EmptyBoxImg />
-      <Heading
-        as="h2"
-        style={{ fontSize: '1.5rem', color: theme.palette.grey300 }}
+    <Main open={false}>
+      <Flex.Column
+        width="100%"
+        height="400px"
+        gap="1rem"
+        justify="center"
+        align="center"
       >
-        {ERROR_MESSAGES.default}
-      </Heading>
-    </Flex.Column>
+        <EmptyBoxImg />
+        <Heading
+          as="h2"
+          style={{ fontSize: '1.5rem', color: theme.palette.grey300 }}
+        >
+          {ERROR_MESSAGES.default}
+        </Heading>
+        <Button
+          label="로그인 페이지로 이동하기"
+          size="large"
+          onClick={() => navigate(ROUTE_PATH.login)}
+        />
+      </Flex.Column>
+    </Main>
   );
 };
 
